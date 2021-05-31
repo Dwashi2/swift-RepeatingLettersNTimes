e77cf40e51b56645f31c23993925708297f81c76
Daniel Washington <danielvertigo@hotmail.com>
2021-05-31 20:10:35 -0300

    Files

--- RepeatingLettersNTimes/ViewController.swift
+++ RepeatingLettersNTimes/ViewController.swift
@@ -5,6 +5,22 @@
 //  Created by Daniel Washington Ignacio on 31/05/21.
 //
 
+
+/*
+ Create a function that repeats each character in a string n times.
+
+ Examples
+
+ repeating("mice", 5) ➞ "mmmmmiiiiiccccceeeee"
+
+ repeating("hello", 3) ➞ "hhheeellllllooo"
+
+ repeating("stop", 1) ➞ "stop"
+ Notes
+
+ N/A
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,6 +28,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    //    print(self.repeating("mice", 5))
+        print(self.repeating("hello", 3))
+      //  print(self.repeating("stop", 1))
+    }
+    
+    func repeating(_ str: String, _ n: Int) -> String {
+        var resultArray: [Character] = []
+        for j in str {
+            for m in 0...n-1 {
+                resultArray.append(j)
+            }
+        }
+        
+        return String(resultArray)
     }
 
 
